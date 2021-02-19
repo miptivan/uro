0d5c09dc6cdad0ec24376cb47c5bde860e556053
Ivan Borisov <miptivan@gmail.com>
2021-02-20 01:40:22 +0500

    initial project version

---
+++ dbworker.py
@@ -0,0 +1,100 @@
+import sqlite3
+import config
+import random
+import openpyxl
+
+wb = openpyxl.reader.excel.load_workbook(filename="C:/Users/mipti/Desktop/table.xlsx")
+wb.active = 0
+sheet = wb.active
+
+db = sqlite3.connect('server.db', check_same_thread=False)
+sql = db.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS users (
+    username TEXT,
+    current_state TEXT,
+    q1 INTEGER,
+    q2 INTEGER,
+    q3 INTEGER,
+    q4 INTEGER,
+    q5 INTEGER,
+    q6 INTEGER,
+    q7 INTEGER,
+    q8 INTEGER,
+    q9 INTEGER,
+    q10 INTEGER
+        
+)""")
+
+
+# Сохраняем текущее «состояние» пользователя в нашу базу
+def set_state(user_id, value):
+    sql.execute(f"SELECT username FROM users WHERE username = '{user_id}'")
+    sqlite_insert_query = f"""UPDATE users SET
+                              current_state = '{value}'
+                              WHERE username = '{user_id}'"""
+    sql.execute(sqlite_insert_query)
+    db.commit()
+
+
+def create_user(user_id):
+    sql.execute(f"SELECT username FROM users WHERE username = '{user_id}'")
+    if sql.fetchone() is None:
+        sql.execute(f"INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (user_id, None, None, None, None, None, None, None, None, None, None, None))  # Создать строчку юзера
+    else:
+        #  sql.execute(f"""UPDATE users SET username = '{user_id}' WHERE username = '{user_id}'""")
+        print('Такой профиль уже есть')
+    db.commit()
+
+
+def get_current_state(user_id):
+    sql.execute(f"SELECT current_state FROM users WHERE username = '{user_id}'")
+    a = sql.fetchone()[0]
+    print(a)
+    return a
+
+
+def set_question(user_id, value, column):
+    sqlite_insert_query = f"""UPDATE users SET
+                                  '{column}' = '{value}'
+                                  WHERE username = '{user_id}'"""
+    sql.execute(sqlite_insert_query)
+    db.commit()
+
+
+def search(user_id):
+    sql.execute(f"SELECT q1 FROM users WHERE username = '{user_id}'")
+    q1 = sql.fetchone()[0]
+    sql.execute(f"SELECT q2 FROM users WHERE username = '{user_id}'")
+    q2 = sql.fetchone()[0]
+    sql.execute(f"SELECT q3 FROM users WHERE username = '{user_id}'")
+    q3 = sql.fetchone()[0]
+    sql.execute(f"SELECT q4 FROM users WHERE username = '{user_id}'")
+    q4 = sql.fetchone()[0]
+    sql.execute(f"SELECT q5 FROM users WHERE username = '{user_id}'")
+    q5 = sql.fetchone()[0]
+    sql.execute(f"SELECT q6 FROM users WHERE username = '{user_id}'")
+    q6 = sql.fetchone()[0]
+    sql.execute(f"SELECT q7 FROM users WHERE username = '{user_id}'")
+    q7 = sql.fetchone()[0]
+    sql.execute(f"SELECT q8 FROM users WHERE username = '{user_id}'")
+    q8 = sql.fetchone()[0]
+    sql.execute(f"SELECT q9 FROM users WHERE username = '{user_id}'")
+    q9 = sql.fetchone()[0]
+    sql.execute(f"SELECT q10 FROM users WHERE username = '{user_id}'")
+    q10 = sql.fetchone()[0]
+    for i in range(1, 3457):
+        if (str(sheet['A' + str(i)].value) == str(q1) and
+                str(sheet['B' + str(i)].value) == str(q2) and
+                str(sheet['C' + str(i)].value) == str(q3) and
+                str(sheet['D' + str(i)].value) == str(q4) and
+                str(sheet['E' + str(i)].value) == str(q5) and
+                str(sheet['F' + str(i)].value) == str(q6) and
+                str(sheet['G' + str(i)].value) == str(q7) and
+                str(sheet['H' + str(i)].value) == str(q8) and
+                str(sheet['I' + str(i)].value) == str(q9) and
+                str(sheet['J' + str(i)].value) == str(q10)):
+            print('EBAAAT ', sheet['k' + str(i)].value)
+            return str(sheet['k' + str(i)].value)
+
+
---
+++ main.py
@@ -0,0 +1,311 @@
+import config
+import telebot
+import dbworker
+
+print("STARTUEM")
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+# S_0
+@bot.message_handler(commands=["start"])
+def cmd_start(message):
+    dbworker.create_user(message.chat.id)
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    keyboard.row('/да', '/нет', '/start')
+    bot.send_message(message.chat.id, "Уважаемый коллега! \n Вам предлагается ознакомиться с рабочим прототипом уникального алгоритма ведения Вашего пациента с ДГПЖ! \n По результату прохождения вопросов этого бота "
+                                      "Вы получить персонифицированные рекомендации по тактики ведения пациента с учётом его текущего состояния на приёме здесь и сейчас.")
+    bot.send_message(message.chat.id, "1. У вашего пациента IPSS больше 7 баллов?", reply_markup=keyboard)
+    dbworker.set_state(message.chat.id, config.States.Q_1.value)
+
+
+# Q_1
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_1.value)
+def user_entering_surname(message):
+    if (message.text == '/да'):
+        dbworker.set_state(message.chat.id, config.States.Q_2.value)
+        dbworker.set_question(message.chat.id, 1, 'q1')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "2. Имеет ли он выраженную ночную полиурию?", reply_markup=keyboard)
+        print('111')
+
+    elif (message.text == '/нет'):
+        dbworker.set_state(message.chat.id, config.States.Q_2.value)
+        dbworker.set_question(message.chat.id, 0, 'q1')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "2. Имеет ли он выраженную ночную полиурию?", reply_markup=keyboard)
+        print('000')
+
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_1.value)
+        print('залупа с 1 вопросом')
+
+
+# Q_2
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_2.value)
+def user_entering_surname(message):
+    if (message.text == '/да'):
+        dbworker.set_state(message.chat.id, config.States.Q_3.value)
+        dbworker.set_question(message.chat.id, 1, 'q2')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/нет', '/нет', '/start')
+        bot.send_message(message.chat.id, "3. Есть только симптомы накопления?", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/нет'):
+        dbworker.set_state(message.chat.id, config.States.Q_3.value)
+        dbworker.set_question(message.chat.id, 0, 'q2')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "3. Есть только симптомы накопления?", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_2.value)
+        print('залупа с 2 вопросом')
+
+
+# Q_3
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_3.value)
+def user_entering_surname(message):
+    if (message.text == '/да'):
+        dbworker.set_state(message.chat.id, config.States.Q_4.value)
+        dbworker.set_question(message.chat.id, 1, 'q3')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "4. Лечение пациента длительное?", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/нет'):
+        dbworker.set_state(message.chat.id, config.States.Q_4.value)
+        dbworker.set_question(message.chat.id, 0, 'q3')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "4. Лечение пациента длительное?", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_3.value)
+        print('залупа с 3 вопросом')
+
+
+# Q_4
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_4.value)
+def user_entering_surname(message):
+    if (message.text == '/да'):
+        dbworker.set_state(message.chat.id, config.States.Q_5.value)
+        dbworker.set_question(message.chat.id, 1, 'q4')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "5. Пациент сексуально активен?", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/нет'):
+        dbworker.set_state(message.chat.id, config.States.Q_5.value)
+        dbworker.set_question(message.chat.id, 0, 'q4')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "5. Пациент сексуально активен?", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_4.value)
+        print('залупа с 4 вопросом')
+
+
+# Q_5
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_5.value)
+def user_entering_surname(message):
+    if (message.text == '/да'):
+        dbworker.set_state(message.chat.id, config.States.Q_6.value)
+        dbworker.set_question(message.chat.id, 1, 'q5')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "6. Есть ли остаточные симптомы накопления?", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/нет'):
+        dbworker.set_state(message.chat.id, config.States.Q_6.value)
+        dbworker.set_question(message.chat.id, 0, 'q5')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/да', '/нет', '/start')
+        bot.send_message(message.chat.id, "6. Есть ли остаточные симптомы накопления?", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_5.value)
+        print('залупа с 5 вопросом')
+
+
+# Q_6
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_6.value)
+def user_entering_surname(message):
+    if (message.text == '/да'):
+        dbworker.set_state(message.chat.id, config.States.Q_7.value)
+        dbworker.set_question(message.chat.id, 1, 'q6')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/после_65', '/до_65', '/start')
+        bot.send_message(message.chat.id, "7. Укажите ваш возраст", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/нет'):
+        dbworker.set_state(message.chat.id, config.States.Q_7.value)
+        dbworker.set_question(message.chat.id, 0, 'q6')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/после_65', '/до_65', '/start')
+        bot.send_message(message.chat.id, "7. Укажите ваш возраст", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_6.value)
+        print('залупа с 6 вопросом')
+
+
+# Q_7
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_7.value)
+def user_entering_surname(message):
+    if (message.text == '/после_65'):
+        dbworker.set_state(message.chat.id, config.States.Q_8.value)
+        dbworker.set_question(message.chat.id, 1, 'q7')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/больше_80мл', '/40_80мл', '/до_40мл', '/start')
+        bot.send_message(message.chat.id, "8. Какой объём простаты пациент имеет?", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/до_65'):
+        dbworker.set_state(message.chat.id, config.States.Q_8.value)
+        dbworker.set_question(message.chat.id, 0, 'q7')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/больше_80мл', '/40_80мл', '/до_40мл', '/start')
+        bot.send_message(message.chat.id, "8. Какой объём простаты пациент имеет?", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_7.value)
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/больше_80мл', '/40_80мл', '/до_40мл', '/start')
+        bot.send_message(message.chat.id, "8. Какой объём простаты пациент имеет?", reply_markup=keyboard)
+        print('залупа с 7 вопросом')
+
+
+# Q_8
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_8.value)
+def user_entering_surname(message):
+    if (message.text == '/больше_80мл'):
+        dbworker.set_state(message.chat.id, config.States.Q_9.value)
+        dbworker.set_question(message.chat.id, 2, 'q8')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/15_более', '/10_15', '/0_10', '/start')
+        bot.send_message(message.chat.id, "9. Какой показатель Qmax по данным УФМ?", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/40_80мл'):
+        dbworker.set_state(message.chat.id, config.States.Q_9.value)
+        dbworker.set_question(message.chat.id, 1, 'q8')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/15_более', '/10_15', '/0_10', '/start')
+        bot.send_message(message.chat.id, "9. Какой показатель Qmax по данным УФМ?", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/до_40мл'):
+        dbworker.set_state(message.chat.id, config.States.Q_9.value)
+        dbworker.set_question(message.chat.id, 0, 'q8')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/15_более', '/10_15', '/0_10', '/start')
+        bot.send_message(message.chat.id, "9. Какой показатель Qmax по данным УФМ?", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_8.value)
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/15_более', '/10_15', '/0_10', '/start')
+        bot.send_message(message.chat.id, "Question 8", reply_markup=keyboard)
+        print('залупа с 8 вопросом')
+
+
+# Q_9
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_9.value)
+def user_entering_surname(message):
+    if (message.text == '/15_более'):
+        dbworker.set_state(message.chat.id, config.States.Q_10.value)
+        dbworker.set_question(message.chat.id, 2, 'q9')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/более_100', '/50-100', '/0_50', '/start')
+        bot.send_message(message.chat.id, "10. Остаточный объем мочи? (выберите диапазон)", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/10_15'):
+        dbworker.set_state(message.chat.id, config.States.Q_10.value)
+        dbworker.set_question(message.chat.id, 1, 'q9')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/2', '/1', '/0', '/start')
+        bot.send_message(message.chat.id, "10. Остаточный объем мочи? (выберите диапазон)", reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/0_10'):
+        dbworker.set_state(message.chat.id, config.States.Q_10.value)
+        dbworker.set_question(message.chat.id, 0, 'q9')
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/2', '/1', '/0', '/start')
+        bot.send_message(message.chat.id, "10. Остаточный объем мочи? (выберите диапазон)", reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_9.value)
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/2', '/1', '/0', '/start')
+        bot.send_message(message.chat.id, "Question 9", reply_markup=keyboard)
+        print('залупа с 9 вопросом')
+
+
+# Q_10
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.Q_10.value)
+def user_entering_surname(message):
+    if (message.text == '/2'):
+        dbworker.set_state(message.chat.id, config.States.S_START.value)
+        dbworker.set_question(message.chat.id, 2, 'q10')
+        result = dbworker.search(message.chat.id)
+        bot.send_message(message.chat.id, result)
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/start')
+        bot.send_message(message.chat.id, 'Вернуться в начало: /start', result)
+        print('111')
+    elif (message.text == '/1'):
+        dbworker.set_state(message.chat.id, config.States.S_START.value)
+        dbworker.set_question(message.chat.id, 1, 'q10')
+        result = dbworker.search(message.chat.id)
+        bot.send_message(message.chat.id, result)
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/start')
+        bot.send_message(message.chat.id, 'Вернуться в начало: /start', reply_markup=keyboard)
+        print('111')
+    elif (message.text == '/0'):
+        dbworker.set_state(message.chat.id, config.States.S_START.value)
+        dbworker.set_question(message.chat.id, 0, 'q10')
+        result = dbworker.search(message.chat.id)
+        bot.send_message(message.chat.id, result)
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/start')
+        bot.send_message(message.chat.id, 'Вернуться в начало: /start', reply_markup=keyboard)
+        print('000')
+    else:
+        dbworker.set_state(message.chat.id, config.States.Q_9.value)
+        # KEYBOARD
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/start')
+        bot.send_message(message.chat.id, "Question 9", reply_markup=keyboard)
+        print('залупа с 10 вопросом')
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
