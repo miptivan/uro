9753fb7eeed2ebab4b3ff74d742ec0ae8aabeeca
Ivan Borisov <miptivan@gmail.com>
2021-02-20 17:25:28 +0500

    add all files

---
+++ config.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+TOKEN = '1675115757:AAExqNf233e4zf6xlPY0kwl_-FbjT19DNF0'
+
+
+class States(Enum):
+    S_START = "0"  # Начало нового диалога
+    Q_1 = "1"
+    Q_2 = "2"
+    Q_3 = "3"
+    Q_4 = "4"
+    Q_5 = "5"
+    Q_6 = "6"
+    Q_7 = "7"
+    Q_8 = "8"
+    Q_9 = "9"
+    Q_10 = "10"
+    Q_11 = "11"
+    S_END = "7"
